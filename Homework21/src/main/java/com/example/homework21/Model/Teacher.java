e0b2268fe191585d5e660acaac98e917666a136a
nadaAlmalki <50924843+nadaAlmalki@users.noreply.github.com>
2023-02-07 20:03:44 +0300

    Add files via upload

---
+++ Homework21/src/main/java/com/example/homework21/Model/Teacher.java
@@ -0,0 +1,42 @@
+package com.example.homework21.Model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Entity
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+@Getter
+@Setter
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "varchar(50) not null")
+    private String name;
+
+    @Column(columnDefinition = "int not null")
+    private Integer age;
+
+
+    @Column(columnDefinition = "varchar(50) not null")
+    private String email;
+
+    @Column(columnDefinition = "int not null")
+    private Integer salary;
+
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "teacher")
+    @PrimaryKeyJoinColumn
+
+    private Address address;
+
+
+}
